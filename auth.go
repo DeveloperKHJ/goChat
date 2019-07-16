36e11b3d814b2d447454b8461b360f738f26211f
DeveloperKHJ <dbtmd1007gh1@gmail.com>
2019-07-16 17:33:52 +0900

    login function with google API

---
+++ auth.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	sessions "github.com/goincremental/negroni-sessions"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+	"github.com/urfave/negroni"
+)
+
+const (
+	nextPageKey     = "next_page" // 세션에 저장되는 next page의 키
+	authSecurityKey = "auth_security_key"
+)
+
+func init() {
+	// gomniauth 정보 세팅
+	gomniauth.SetSecurityKey(authSecurityKey)
+	gomniauth.WithProviders(
+		google.New("212072750586-i12ppbgv86o33an3bkbsj9dfc1pol0ui.apps.googleusercontent.com", "lwVaJ1rOnoxxBDbVhPBQjLjC",
+			"http://127.0.0.1:3000/auth/callback/google"),
+	)
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	action := ps.ByName("action")
+	provider := ps.ByName("provider")
+	s := sessions.GetSession(r)
+
+	switch action {
+	case "login":
+		// gomniauth.Provider의 login 페이지로 이동
+		p, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		loginUrl, err := p.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		http.Redirect(w, r, loginUrl, http.StatusFound)
+	case "callback":
+		// gomniauth 콜백 처리
+		p, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		creds, err := p.CompleteAuth(objx.MustFromURLQuery(r.URL.RawQuery))
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		// 콜백 결과로부터 사용자 정보 확인
+		user, err := p.GetUser(creds)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		u := &User{
+			Uid:       user.Data().Get("id").MustStr(),
+			Name:      user.Name(),
+			Email:     user.Email(),
+			AvatarUrl: user.AvatarURL(),
+		}
+
+		SetCurrentUser(r, u)
+		http.Redirect(w, r, s.Get(nextPageKey).(string), http.StatusFound)
+	default:
+		http.Error(w, "Auth action '"+action+"' is not supported", http.StatusFound)
+	}
+}
+
+func LoginRequired(ignore ...string) negroni.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
+		// ignore URL이면 다음 핸들러 실행
+		for _, s := range ignore {
+			if strings.HasPrefix(r.URL.Path, s) {
+				next(w, r)
+				return
+			}
+		}
+		// CurrentUser 정보를 가져옴
+		u := GetCurrentUser(r)
+
+		// CurrentUser 정보가 유효하면 만료 시간을 갱신하고 다음 핸들러 실행
+		if u != nil && u.Valid() {
+			SetCurrentUser(r, u)
+			next(w, r)
+			return
+		}
+
+		// CurrentUser 정보가 유효하지 않으면 CurrentUser를 nil로 세팅
+		SetCurrentUser(r, nil)
+
+		// 로그인 후 이동항 URL을 세션에 저장(r)
+		sessions.GetSession(r).Set(nextPageKey, r.URL.RequestURI())
+
+		// 로그인 페이지로 리다이렉트
+		http.Redirect(w, r, "/login", http.StatusFound)
+	}
+}
